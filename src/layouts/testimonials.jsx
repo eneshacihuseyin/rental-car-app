569038a0aafccdf04a316f900b3c1fd78601a208
eneshacihuseyin <e.hacihuseyin03@gmail.com>
2025-04-11 23:10:07 +0300

    Added Features and Testimonials Sections

---
+++ src/layouts/testimonials.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+function Testimonials() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="w-full h-[45vh] flex p-[2vh] gap-[2vh]">
+      <div className="flex-1 p-[2vh]  dark:bg-[#212128] bg-[#E8E8EF] flex flex-col ">
+        <div className="flex-[1] px-[3vw] flex justify-start gap-[3vh]">
+          <div className=" size-[14vh]">
+            <img
+              src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"
+              alt=""
+              className="rounded-full"
+            />
+          </div>
+          <div className="pt-[1vh]">
+            <div className="text-xl">John Doe</div>
+            <div className="rating h-[3vh]">
+              <img src="/five-star.png" alt="" className="h-full" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-[3] p-[3vh] text-justify">{t('home.testimonials.comment1')}</div>
+      </div>
+      <div className="flex-1 p-[2vh] dark:bg-[#212128] bg-[#E8E8EF] flex flex-col ">
+        <div className="flex-[1] px-[3vw] flex justify-start gap-[3vh]">
+          <div className=" size-[14vh]">
+            <img
+              src="https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://media.easy-peasy.ai/78e88f76-29bf-4289-9624-719aec0f7bcb/e516f677-4846-4a28-9707-ba00ffa49479.png"
+              alt=""
+              className="rounded-full"
+            />
+          </div>
+          <div className="pt-[1vh]">
+            <div className="text-xl">Jane Doe</div>
+            <div className="rating h-[3vh]">
+              <img src="/four-star.png" alt="" className="h-full" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-[3] p-[3vh] text-justify">{t('home.testimonials.comment2')}</div>
+      </div>
+      <div className="flex-1 p-[2vh] dark:bg-[#212128] bg-[#E8E8EF] flex flex-col ">
+        <div className="flex-[1] px-[3vw] flex justify-start gap-[3vh]">
+          <div className=" size-[14vh]">
+            <img
+              src="https://www.profilebakery.com/wp-content/uploads/2023/04/LINKEDIN-Profile-Picture-AI.jpg"
+              alt=""
+              className="rounded-full"
+            />
+          </div>
+          <div className="pt-[1vh]">
+            <div className="text-xl">Michael Smith</div>
+            <div className="rating h-[3vh]">
+              <img src="/five-star.png" alt="" className="h-full" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-[3] p-[3vh] text-justify">{t('home.testimonials.comment3')}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '@/layouts/hero.jsx';
+import Features from '@/layouts/features.jsx';
+import Testimonials from '@/layouts/testimonials.jsx';
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Features />
+      <Testimonials />
     </div>
   );
 }
